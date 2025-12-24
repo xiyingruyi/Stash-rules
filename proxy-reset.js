383a8d438b1009fd7128bd5fa6555f2fef105893
xiyingruyi <88181161+xiyingruyi@users.noreply.github.com>
2025-12-25 00:49:34 +0800

    Update proxy-reset.js

--- proxy-reset.js
+++ proxy-reset.js
@@ -3,18 +3,20 @@ function operator(proxies) {
     // Hy2：Hysteria2 类型
     if (proxy.type === 'hysteria2') {
       proxy.server = 'hy2.xinote.site';
+      proxy.ports = "30000-50000";
+      delete proxy.port;  // 删除原有的 port 字段，避免冲突
     }
-    
+   
     // Reality：VLESS + Reality 类型（有 reality-opts）
     else if (proxy.type === 'vless' && proxy['reality-opts']) {
       proxy.server = 'reality.xinote.site';
     }
-    
+   
     // Trojan 类型
     else if (proxy.type === 'trojan') {
       proxy.server = 'trojan.xinote.site';
     }
-    
+   
     // vless-upgrade：VLESS + WS + v2ray-http-upgrade
     else if (
       proxy.type === 'vless' &&
@@ -24,7 +26,7 @@ function operator(proxies) {
     ) {
       proxy.server = 'vless.xinote.site';
     }
-    
+   
     // vless-ws：普通 VLESS + WS（无 upgrade）
     else if (
       proxy.type === 'vless' &&
@@ -33,7 +35,7 @@ function operator(proxies) {
     ) {
       proxy.server = 'ws.xinote.site';
     }
-    
+   
     return proxy;
   });
 }
